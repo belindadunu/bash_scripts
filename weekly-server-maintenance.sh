a83101223467dfdf7379b3d008cd5bf8826cc2c9
Belinda Dunu <139175163+belindadunu@users.noreply.github.com>
2023-09-04 10:44:46 -0400

    Create weekly-server-maintenance.sh

---
+++ weekly-server-maintenance.sh
@@ -0,0 +1,26 @@
+# bash-script-weekly-server-maintenance
+
+Purpose: This script is to run as a cronjob every Friday at 11pm to update the server and log the output.
+
+By: Belinda Dunu
+
+Date: August 30, 2023
+
+
+**#!/bin/bash**
+
+**_Update the server packages_**  
+
+`$ sudo apt update` 
+
+**_Get the number of packages that can be upgraded_** 
+
+`$ sudo apt list --upgradable` 
+
+`$ upgrades=$apt(list --upgradeable)` 
+
+**_Create a dated log file and output the number of upgradable packages_**
+
+`$ echo "$upgrades packages can be upgraded" > update$(date +%m.%d.%y).txt`
+
+**_Edit the crontab config_** 
